b77e917991fb67dff549a68b99e4d1a2fb8ca1ea
woj <woj.swiderski@gmail.com>
2018-11-21 22:06:44 +0100

    table improvements

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@
 // const msg = document.getElementById('msg');
 // msg.innerHTML = document.getElementsByClassName("fas fa-bars").length;
 
+
+
 let menuIsHidden = false;
 document.getElementsByClassName("fas fa-bars")[0].addEventListener('click', menuHider);
+
+window.onload = function(){
+    const site = window.location.href;
+    document.getElementById('msg').innerHTML = site + ' ' + site.indexOf('index.html');
+    if (site.indexOf('index.html') != -1) {
+        document.getElementById('general').style.borderLeft = '4px solid #f58220'; 
+    }
+}
+
+
 
 function menuHider(){
     const a_menu = document.getElementById('menu');
